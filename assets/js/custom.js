eac5212e9a5bd8d155481cff072a9b12181f836e
Rahmat Wahyu Triessaputra <rahmatwahyutriessaputra@gmail.com>
2022-01-28 22:09:25 +0700

    create input form to db

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,5 @@
 const API_TOKEN = "asd"
-const API_URL = "https://script.google.com/macros/s/AKfycbz9d4pdPaTlp3fkQjPIiOZ2Rimr69lg7wzPz85_9cZ3ASI6BF8M6PPKZLRccXrZ76t9/exec"
+const API_URL = "https://script.google.com/macros/s/AKfycbwFivaCGkG8hqREjwjK9d1nhgVD5V6ZYsDi-jaxpjKYjEuweTbllovlfz6z9YYgoJzi/exec"
 
 function httpGet(theUrl){
     var xmlHttp = new XMLHttpRequest();
@@ -12,6 +12,11 @@ function setValueToElement(id, val){
     document.getElementById(id).innerHTML = val
 }
 
+function display(id, val){
+    document.getElementById(id).style.display = val;
+
+}
+
 function currencyFormatter(input){
     const formatter = new Intl.NumberFormat('id-ID', 
     { style: 'currency', 
@@ -22,8 +27,26 @@ function currencyFormatter(input){
  }
 
  function dateFormatter(input){
-    const formatter = new Date(input)
+    const newDate = new Date(input)
+    const listOfMonth = ["Januari","Februari","Maret","April","May","Juni","Juli","Agustus","September","Oktober","November","Desember"];
+    const listOfDay = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+
+    var year = newDate.getFullYear()
+    var month = listOfMonth[newDate.getMonth()];
+    var date = newDate.getDate()
+    var day = listOfDay[newDate.getDay()];
+
+    var formatter = `${day}, ${date} ${month} ${year}`
+    return formatter
  }
+
+function notification(status, header, msg){
+    Swal.fire(
+        header,
+        msg,
+        status
+      )
+}
 
 function getPriorityTrainingData(){
     var data = httpGet(`${API_URL}?token=${API_TOKEN}&priority=true`)
@@ -35,7 +58,6 @@ function getPriorityTrainingData(){
                     <h4>${r.name}</h4>
                     <p>${r.category}</p>
                     <div class="portfolio-links">
-                      <a href="${r.media_2}" data-gallery="portfolioGallery" class="portfokio-lightbox" title="${r.name}"><i class="bi bi-plus"></i></a>
                       <a href="portfolio-details.html?id=${r.id}" title="More Details"><i class="bi bi-link"></i></a>
                     </div>
                   </div>
@@ -44,6 +66,12 @@ function getPriorityTrainingData(){
       
       document.getElementById('data-classes').innerHTML += res;
     })
+}
+
+function getParamsFromURL(url, params){
+    var url = new URL(url)
+    var result = url.searchParams.get(params);
+    return result
 }
 
 function getTrainingDataByID(){
@@ -61,9 +89,51 @@ function getTrainingDataByID(){
         document.getElementById('image-slider').innerHTML += res;
         setValueToElement('t-name', r.name)
         setValueToElement('t-name-2', r.name)
+        setValueToElement('t-name-3', r.name)
         setValueToElement('t-category', r.category)
         setValueToElement('t-price', currencyFormatter(r.price))
-        setValueToElement('t-created', r.created_at)
+        setValueToElement('t-created', dateFormatter(r.created_at))
         setValueToElement('t-description', r.description)
+        document.getElementById('training-id').value = id
+        document.getElementById('training-name').value = r.name
     })
 }
+
+document.getElementById('register-training').addEventListener('submit', function (event) {
+
+	event.preventDefault();
+    display('submit-button', 'none')
+    
+    var serializeForm = function (form) {
+        var obj = {};
+        var formData = new FormData(form);
+        for (var key of formData.keys()) {
+            obj[key] = formData.get(key);
+        }
+        return obj;
+    };
+
+	fetch(API_URL+"?target=register", {
+		method: 'POST',
+		body: JSON.stringify(serializeForm(event.target)),
+		headers: {
+			'Content-type': 'text/plain;charset=utf-8'
+		}
+	}).then(function (response) {
+		if (response.ok) {
+			return response.json();
+		}
+		return Promise.reject(response);
+	}).then(function (data) {
+		notification('success', "Sukses!", "Terimakasih. Data anda sudah berhasil kami terima. Tim kami akan segera menghubungi anda.")
+        document.getElementById("register-training").reset();
+        display('submit-button', 'block')
+        setTimeout(function(){
+            location.reload()
+        }, 3000);
+	}).catch(function (error) {
+		console.warn(error);
+		notification('danger', "Oops!", "Terjadi kesalahan, silahkan coba lagi.")
+        display('submit-button', 'block')
+	});
+});
